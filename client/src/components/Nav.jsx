80bc10a7c31a709448aa0de650e9359e261eae56
SvitlanaMOliinyk <s.m.oliinyk@gmail.com>
2023-02-14 15:34:13 +0100

    Responsive Navbar

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,18 +1,104 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { NavLink, Link } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
+import logo from "../assets/logo.png";
+import "./nav.css";
 
 import TEST_ID from "./Nav.testid";
 
 const Nav = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const handleClick = () => setToggle(!toggle);
+  const shutMenu = () => setToggle(false);
+
   return (
-    <ul>
-      <Link to="/" data-testid={TEST_ID.linkToHome}>
-        <li>Home</li>
-      </Link>
-      <Link to="/user" data-testid={TEST_ID.linkToUsers}>
-        <li>Users</li>
-      </Link>
-    </ul>
+    <header className="logoAndNavbar">
+      <nav className="nav">
+        <div className="logo-navbar-container">
+          <Link to="/" onClick={shutMenu}>
+            <img className="logo" src={logo} alt="Logo" width={100} />
+          </Link>
+          <div className="burger-menu-icon" onClick={handleClick}>
+            {toggle ? <FaTimes /> : <FaBars />}
+          </div>
+          <ul className={toggle ? "navbar active" : "navbar"}>
+            <li className="navbar-item">
+              <NavLink
+                to="/"
+                data-testid={TEST_ID.linkToHome}
+                style={({ isActive }) => {
+                  return {
+                    backgroundColor: isActive ? "var(--active-navlink-color)" : "",
+                    borderRadius: isActive ? "4px" : ""
+                  };
+                }}
+                onClick={shutMenu}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li className="navbar-item">
+              <NavLink
+                to="museums"
+                style={({ isActive }) => {
+                  return {
+                    backgroundColor: isActive ? "var(--active-navlink-color)" : "",
+                    borderRadius: isActive ? "4px" : ""
+                  };
+                }}
+                onClick={shutMenu}
+              >
+                Museums
+              </NavLink>
+            </li>
+            <li className="navbar-item">
+              <NavLink
+                to="favorites"
+                style={({ isActive }) => {
+                  return {
+                    backgroundColor: isActive ? "var(--active-navlink-color)" : "",
+                    borderRadius: isActive ? "4px" : ""
+                  };
+                }}
+                onClick={shutMenu}
+              >
+                Favorites
+              </NavLink>
+            </li>
+            <li className="navbar-item">
+              <NavLink
+                to="offers"
+                style={({ isActive }) => {
+                  return {
+                    backgroundColor: isActive ? "var(--active-navlink-color)" : "",
+                    borderRadius: isActive ? "4px" : ""
+                  };
+                }}
+                onClick={shutMenu}
+              >
+                Offers
+              </NavLink>
+            </li>
+            <li className="navbar-item">
+              <NavLink
+                to="/user"
+                data-testid={TEST_ID.linkToUsers}
+                style={({ isActive }) => {
+                  return {
+                    backgroundColor: isActive ? "var(--active-navlink-color)" : "",
+                    borderRadius: isActive ? "4px" : ""
+                  };
+                }}
+                onClick={shutMenu}
+              >
+                Log in
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
   );
 };
 
