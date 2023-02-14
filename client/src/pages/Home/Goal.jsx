cc51eb4e3e9e3212a499a032866916cd38562f44
Yasserhub <yasser.a.murshed@gmail.com>
2023-02-14 22:43:58 +0100

    update the goal section based on the feedback

--- client/src/pages/Home/Goal.jsx
+++ client/src/pages/Home/Goal.jsx
@@ -1,81 +1,36 @@
 import React from "react";
-import { AiOutlineAim } from "react-icons/ai";
+// import { AiOutlineAim } from "react-icons/ai";
+import "./goal.css";
 
-import { MdFeaturedPlayList } from "react-icons/md";
-MdFeaturedPlayList;
+// import { MdFeaturedPlayList } from "react-icons/md";
 
 const Goal = () => {
   return (
-    <main>
-      <img
-        src="https://res.cloudinary.com/diyopzdxb/image/upload/v1676304771/cars-images/68592_fullimage_frans_hals_rendezvous_1360x430_0_m6jwb4.jpg"
-        alt="contact with us"
-      ></img>
-      <div className="goalContainer">
-        <div className="purpose">
-          <h3>
-            The Purpose of{" "}
-            <span className="museumName">Find Museum Website</span>
-          </h3>
-          <p>And its Features</p>
-        </div>
+    <div className="container">
+      <div className="goal">
+        <span className="question">
+          Do you want to learn more about the all museums and their collections
+          in Netherlands?
+        </span>
+        <br />
+        <span className="answer">
+          FIND MUSEUM is the right application for that, where you can also
+          follow upcoming events, read reviews, and share your experiences.
+        </span>
 
-        <div className="message">
-          <div className="icon objective">
-            {" "}
-            <AiOutlineAim size={70} />
-          </div>
-          <ul>
-            <li>
-              {" "}
-              The website aims to provide information about the Museums and
-              their collections in Netherlands.
-            </li>
-            <li>
-              It aims also to promote and market Museums to the potential
-              visitors.
-            </li>
-          </ul>
-        </div>
-
-        <div className="appFeatures">
-          <div className="icon features">
-            {" "}
-            <MdFeaturedPlayList size={70} />
-          </div>
-          <ul>
-            <li>
-              The website enables visitors to{" "}
-              <a href="#" className="focus">
-                search for Museums{" "}
-              </a>
-              with different options including name, city, price, ...
-            </li>
-            <li>
-              The website provides visitors with{" "}
-              <a href="#" className="focus">
-                upcoming events
-              </a>{" "}
-              in all Museums.
-            </li>
-            <li>
-              The website provides visitors with{" "}
-              <a href="#" className="focus">
-                special offers
-              </a>{" "}
-              Museums.
-            </li>
-            <li>
-              The website saves{" "}
-              <a href="#" className="focus">
-                favorite Museums
-              </a>{" "}
-              of visitors.
-            </li>
-          </ul>
-        </div>
+        <span className="answer">
+          {" "}
+          Don’t forget to get{" "}
+          <a href="#" className="focus">
+            the special offers
+          </a>
+          , just for you!
+        </span>
+        <button className="button about" type="button">
+          About Us
+        </button>
       </div>
-    </main>
+    </div>
   );
 };
 export default Goal;
