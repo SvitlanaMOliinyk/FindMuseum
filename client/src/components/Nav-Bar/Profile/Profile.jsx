e25cd094768c5d9eeee7f8bbbc859d7c10986b70
Elif Nur Kalkan <elifnkalkan@gmail.com>
2023-02-24 20:11:37 +0100

    log-out bug fixed

--- client/src/components/Nav-Bar/Profile/Profile.jsx
+++ client/src/components/Nav-Bar/Profile/Profile.jsx
@@ -24,7 +24,7 @@ const Profile = ({ onClose }) => {
         <Link
           to={props.link}
           onClick={() => {
-            props.logout;
+            props.logout();
             onClose();
           }}
         >
