728354b008ddf87b50fdb36e9658805cae528e7b
trebektas <trensarbektas@gmail.com>
2023-03-13 17:16:54 +0100

    fix css bugs for forgot password responsiveness

--- server/src/util/mailer.js
+++ server/src/util/mailer.js
@@ -17,12 +17,34 @@ export const sendMail = async (firstName, email) => {
       from: process.env.SENDER_EMAIL,
       to: email,
       subject: "Offer winner",
-      html: `
-    <h1>Hello dear ${firstName}</h1>
-    <p>Congratulations! Your offer code is ${randomCode}!</p>
-    <p>Have a nice time and do not forget to share your impressions with us.</p>
-    <p>Sincerely, Find Museum team</p>
-    `,
+      html: `<!DOCTYPE html>
+      <html lang="en" >
+      <head>
+        <meta charset="UTF-8">
+        <title>Offer Winner</title>
+        
+      </head>
+      <body>
+      
+      <div style="font-family: Helvetica,Arial,sans-serif;max-width:600px;overflow:auto;line-height:2">
+        <div style="margin:50px auto;padding:20px 0">
+          <div style="border-bottom:1px solid #eee">
+            <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">Find Museum</a>
+          </div>
+          <p style="font-size:1.1em">Hello dear ${firstName},</p>
+          <p>Congratulations! Your offer code is <b>${randomCode}</b>!</p>
+          <p>Have a nice time and do not forget to share your impressions with us.</p>
+          <p style="font-size:0.9em;">Sincerely, Find Museum team</p>
+          <hr style="border:none;border-top:1px solid #eee" />
+          <div style="float:left;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
+            <p>If you wish to cancel our special offer, please contact us via e-mail or phone.</p>
+          </div>
+        </div>
+      </div>
+      <!-- partial -->
+        
+      </body>
+      </html>`,
     });
     logInfo(info.messageId);
   } catch (error) {
